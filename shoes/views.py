674c40b986e15a15154988489b2370abf07add8c
Rattnak-013 <215679120+Rattnak-007@users.noreply.github.com>
2025-08-15 00:55:48 +0700

    Limit commit

--- shoes/views.py
+++ shoes/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.pagination import LimitOffsetPagination
 from .models import (
     Brand, Category, Shoe, Customer, Address, Order, OrderItem,
     Review, Wishlist, Cart, CartItem
@@ -12,46 +13,61 @@ from .serializers import (
 
 # Create your views here.
 
+class StandardResultsSetPagination(LimitOffsetPagination):
+    default_limit = 2
+    max_limit = 3
+
 class BrandViewSet(viewsets.ModelViewSet):
     queryset = Brand.objects.all()
     serializer_class = BrandSerializer
+    pagination_class = StandardResultsSetPagination
 
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = StandardResultsSetPagination
 
 class ShoeViewSet(viewsets.ModelViewSet):
     queryset = Shoe.objects.all()
     serializer_class = ShoeSerializer
+    pagination_class = StandardResultsSetPagination
 
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    pagination_class = StandardResultsSetPagination
 
 class AddressViewSet(viewsets.ModelViewSet):
     queryset = Address.objects.all()
     serializer_class = AddressSerializer
+    pagination_class = StandardResultsSetPagination
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    pagination_class = StandardResultsSetPagination
 
 class OrderItemViewSet(viewsets.ModelViewSet):
     queryset = OrderItem.objects.all()
     serializer_class = OrderItemSerializer
+    pagination_class = StandardResultsSetPagination
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    pagination_class = StandardResultsSetPagination
 
 class WishlistViewSet(viewsets.ModelViewSet):
     queryset = Wishlist.objects.all()
     serializer_class = WishlistSerializer
+    pagination_class = StandardResultsSetPagination
 
 class CartViewSet(viewsets.ModelViewSet):
     queryset = Cart.objects.all()
     serializer_class = CartSerializer
+    pagination_class = StandardResultsSetPagination
 
 class CartItemViewSet(viewsets.ModelViewSet):
     queryset = CartItem.objects.all()
     serializer_class = CartItemSerializer
+    pagination_class = StandardResultsSetPagination
