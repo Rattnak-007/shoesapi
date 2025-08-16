f8950f26d7e8a4af7a7d8c7bec03882cec82f7b6
Rattnak-013 <215679120+Rattnak-007@users.noreply.github.com>
2025-08-16 14:47:04 +0700

    commit Shoes

--- shoes/models.py
+++ shoes/models.py
@@ -19,8 +19,7 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
-
-
+    
 class Shoe(models.Model):
     name = models.CharField(max_length=200)
     brand = models.ForeignKey(Brand, on_delete=models.CASCADE, related_name='shoes')
